36b42967846da29cb6db93687d713e3bd210f68b
Ramsey Chinedu <ramseychinedu@gmail.com>
2024-01-12 10:25:41 +0100

    Social media front-end project with React

---
+++ src/components/post/Post.jsx
@@ -0,0 +1,44 @@
+import { MoreVert } from "@mui/icons-material";
+import "./post.css";
+import { Users } from "../../dummyData";
+import { useState } from "react";
+
+export default function Post({post}) {
+    const [like, setLike] = useState(post.like);
+    const [isliked, setisLiked] = useState(false);
+
+    const likeHandler = ()=>{
+        setLike(isliked ? like-1 : like+1)
+        setisLiked(!isliked)
+    }
+  return (
+    <div className="post">
+        <div className="postWrapper">
+            <div className="postTop">
+                <div className="postTopLeft">
+                    <img src={Users.filter(u=>u.id === post.userId)[0].profilePicture} alt="" className="postProfileImg" />
+                    <span className="postUsername">{Users.filter(u=>u.id === post.userId)[0].username}</span>
+                    <span className="postDate">{post.date}</span>
+                </div>
+                <div className="postTopright">
+                    <MoreVert/>
+                </div>
+            </div>
+            <div className="postCenter">
+                <span className="postText">{post?.desc}</span>
+                <img className="postImg" src={post.photo} alt="" />
+            </div>
+            <div className="postBottom">
+                <div className="postBottomLeft">
+                    <img className="likeIcon" src="/assets/like.png" alt="" onClick={likeHandler}/>
+                    <img className="likeIcon" src="/assets/heart.png" alt="" onClick={likeHandler}/>
+                    <span className="postLikeCounter">{like} people</span>
+                </div>
+                <div className="postBottomRight">
+                    <div className="postCommentText">{post.comment} comments</div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
